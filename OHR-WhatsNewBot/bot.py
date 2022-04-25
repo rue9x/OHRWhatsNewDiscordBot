e556de94b95ae03add536aa5941637fd2e7a0607
Rue Lazzaro <rue.lazzaro@gmail.com>
2022-04-24 20:54:34 -0700

    Add files via upload
    Added !nightly_changes command. Allows you to just see the changes to the nightly whatsnew (not the whole nightly release, just what's changed since the last time !nightly_changes or !whatsnew nightly has been run)
    
    APP_TOKEN is now held in config.json.

--- OHR-WhatsNewBot/bot.py
+++ OHR-WhatsNewBot/bot.py
@@ -1,32 +1,79 @@
-'''
-OHRRPGCE Whatsnew.txt discord bot
-2022 Rue Lazzaro
-for the Slime Salad community
-
-'''
-
-
 import discord
 from urllib.request import urlopen
 import textwrap
 import time
 from discord.ext import commands
+import os
+import json
 
 NIGHTLY_WHATSNEW_URL = "https://raw.githubusercontent.com/ohrrpgce/ohrrpgce/wip/whatsnew.txt"
 RELEASE_WHATSNEW_URL = "https://hamsterrepublic.com/ohrrpgce/whatsnew.txt"
-APP_TOKEN = "" # Get your own app token. :)
+APP_TOKEN = ""
+SAVE_FOLDER = os.path.dirname(os.path.realpath(__file__))
 MAX_CONTENT_LENGTH = 2000-6 # -6 to supply block formatting.
 MSG_DELAY = 3
+END_OF_UPDATE_TEXT = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 client = commands.Bot(command_prefix='!')
 
-def get_whatsnew(url):
-    content = urlopen(url).read()
-    content = (content.decode('utf-8'))
-    content = content.split("*** New Features")
-    content = content[0] + content[1] # Should just be the most recent release/WIP release.
-    split_string = textwrap.wrap(content, MAX_CONTENT_LENGTH,replace_whitespace=False,break_long_words=False)
-    del split_string[-1]
-    return split_string
+with open (os.path.join(SAVE_FOLDER,"config.json"),'r') as fi:
+    CONFIG = json.load(fi)
+
+APP_TOKEN = CONFIG["APP_TOKEN"]
+
+def save_whatsnew(url=None,text_content=None):
+    if url == NIGHTLY_WHATSNEW_URL:
+        fn = "nightly.txt"
+    if url == RELEASE_WHATSNEW_URL:
+        fn = "release.txt"
+    
+    with open (os.path.join(SAVE_FOLDER, fn),'w') as fo:
+        for each_line in text_content: # this is a list
+            fo.write(each_line)
+        print ("Updated "+fn)
+
+
+def get_whatsnew(url=None,save_update=False):
+    if url == NIGHTLY_WHATSNEW_URL or url == NIGHTLY_WHATSNEW_URL:
+        content = urlopen(url).read()
+        content = (content.decode('utf-8'))
+        content = content.split("*** New Features")
+        content = content[0] + content[1] # Should just be the most recent release/WIP release.
+        split_string = textwrap.wrap(content, MAX_CONTENT_LENGTH,replace_whitespace=False,break_long_words=False)
+        for each_text in END_OF_UPDATE_TEXT:
+            if each_text in split_string[-1].lower():
+                del split_string[-1]
+        
+        if save_update == True:
+            save_whatsnew(url=url,text_content=split_string)
+        
+        return split_string
+    else:
+        return None
+
+def get_just_changes(url=None):
+    if url == NIGHTLY_WHATSNEW_URL:
+        fn = "nightly.txt"
+    elif url == RELEASE_WHATSNEW_URL:
+        fn = "release.txt"
+    else:
+        return None
+    
+    with open (os.path.join(SAVE_FOLDER, fn),'r') as fi:
+        old_whatsnew = fi.readlines()
+
+    get_whatsnew(url=url,save_update=True)
+
+    with open (os.path.join(SAVE_FOLDER, fn),'r') as fi:
+        new_whatsnew = fi.readlines()
+
+    new_updates = list()
+    for each in new_whatsnew:
+        if each in old_whatsnew:
+            pass
+        else:
+            new_updates.append(each)
+
+    return new_updates
 
 
 @commands.cooldown(rate=1, per=30, type=commands.BucketType.guild)
@@ -40,13 +87,23 @@ async def whatsnew(ctx,release_or_nightly:str):
 
     if release_or_nightly.lower() == "release" or release_or_nightly.lower() == "nightly":
     # SENDS BACK A MESSAGE TO THE CHANNEL.
-        msg = get_whatsnew(url_to_use)
+        msg = get_whatsnew(url=url_to_use,save_update=True)
         await ctx.send(f"{release_or_nightly} Whatsnew: {url_to_use}\n----------")
         for each in msg: 
             await ctx.send("```"+each+"```")
             time.sleep(MSG_DELAY)
 
-
+@commands.cooldown(rate=1, per=30, type=commands.BucketType.guild)
+@client.command(name='nightly_updates',help="Just displays and updates what is new in the update, not the entire nightly.")
+async def nightly_updates(ctx):
+    msg = get_just_changes(url=NIGHTLY_WHATSNEW_URL)
+    if len(msg) < 1:
+        await ctx.send(f"No changes in nightly, sorry!")
+    else:
+        await ctx.send(f"Nightly changes\n----------")    
+        for each in msg: 
+            await ctx.send(each)
+            time.sleep(MSG_DELAY)
 
 
 @client.event
