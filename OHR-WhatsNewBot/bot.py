33a915848d449a7722ff2bd45cb2c9ea4eae87d8
Rue Lazzaro <rue.lazzaro@gmail.com>
2023-06-03 17:06:21 -0400

    Cooldown added (defaults to 120 seconds). Moved many global variables to config.json. Included an example_config.json. Included a requirements.txt. Updated README.md with new usage.

--- OHR-WhatsNewBot/bot.py
+++ OHR-WhatsNewBot/bot.py
@@ -1,4 +1,3 @@
-from urllib.request import urlopen
 import discord
 from discord.ext import commands
 import os
@@ -6,17 +5,25 @@ import json
 import urllib.request
 import textwrap
 import re
-NIGHTLY_WHATSNEW_URL = "https://raw.githubusercontent.com/ohrrpgce/ohrrpgce/wip/whatsnew.txt"
-RELEASE_WHATSNEW_URL = "https://hamsterrepublic.com/ohrrpgce/whatsnew.txt"
-APP_TOKEN = ""
-SAVE_FOLDER = ""
-MSG_SIZE = 1976
 
+
+SAVE_FOLDER = ""
+SAVE_FOLDER = os.getcwd() + os.sep
+
+# Globals are loaded from config.
 with open (os.path.join(SAVE_FOLDER,"config.json"),'r') as fi:
     CONFIG = json.load(fi)
 APP_TOKEN = CONFIG["APP_TOKEN"]
+NIGHTLY_WHATSNEW_URL = CONFIG["NIGHTLY_WHATSNEW_URL"]
+RELEASE_WHATSNEW_URL = CONFIG["RELEASE_WHATSNEW_URL"] 
+COOLDOWN_TIME = CONFIG["COOLDOWN_TIME"]
+MSG_SIZE = CONFIG["MSG_SIZE"]
 
 def compare_release_notes(old_notes, new_notes):
+    '''
+    Takes old_notes (previous release's whatsnew.txt) and the new_notes (nightly whatsnew.txt).
+    Returns a list of lines displaying only whats new in the nightly release.
+    '''
     # Read the contents of the old release notes
     with open(old_notes, 'r') as old_file:
         old_lines = [line.strip() for line in old_file.readlines() if line.strip()]
@@ -25,7 +32,6 @@ def compare_release_notes(old_notes, new_notes):
     with open(new_notes, 'r') as new_file:
         new_lines = [line.strip() for line in new_file.readlines() if line.strip()]
 
-    # Iterate over the new release notes and print new changes
     retval = ""
 
     # Define common date formats to match
@@ -43,44 +49,54 @@ def compare_release_notes(old_notes, new_notes):
     # Define a pattern to match common date formats
     date_pattern = r"\b(\w+ \d{1,2}(?:st|nd|rd|th)?,? \d{4})\b"
 
-    i = 0
+    i = 0 # For counting how many times we've seen a date.
     for line in new_lines:  
         edit_line = line
-        if "***" in edit_line:
+        if "***" in edit_line: # Add some extra formatting for new sections (which start with ***)
             edit_line = "\n\n"+edit_line
 
         match = re.search(date_pattern, edit_line)
         if match != None:
             if i == 0 :
                 retval = retval + edit_line.replace(match.group(0),"") + "\n"
-                i = i + 1
+                i = i + 1 # We allow the first date in the file to show up (as it is the new update)
             if i > 0:
+                # We don't allow any more dates. Second time we see a date it means we're in the previous update.
                 return retval
         
         if edit_line not in old_lines:
+            # Compare the old release with the new release. If it's new, add it.
+            # We use edit_line to ensure the new formatted lines end up in the update.
             retval = retval + edit_line + "\n"       
+    
     return retval
 
 
 def save_from_url(url, file_path):
+    ''' 
+    Takes a url (preferably a whatsnew.txt) and file_path (where to save it).
+    '''
     try:
         urllib.request.urlretrieve(url, file_path)
         print(f"File downloaded successfully and saved as {file_path}")
     except Exception as e:
         print(f"Error occurred while downloading the file: {str(e)}")
 
+
+
+# Discord setup:
 intents = discord.Intents.all()
 intents.typing = False  # Disable typing events to reduce unnecessary event handling
-
-allowed_channels = [967322168511827972]  # Replace with the desired channel IDs
+allowed_channels = list(CONFIG["ALLOWED_CHANNELS"])  # Replace with the desired channel IDs
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
+    print ("Started OHR WhatsNew Bot")
     print("------")
 
 @bot.command()
+@commands.cooldown(1, COOLDOWN_TIME, commands.BucketType.user)
 async def whatsnew(ctx):
     if ctx.channel.id not in allowed_channels:
         await ctx.send("This command is not allowed in this channel.")
@@ -93,7 +109,7 @@ async def whatsnew(ctx):
     # Call the compare_release_notes function
     output_message = compare_release_notes(old_release_notes_file, new_release_notes_file)
     # Split the output message into chunks at word boundaries
-    wrapped_message = textwrap.wrap(output_message, width=MSG_SIZE, break_long_words=False,replace_whitespace=False)
+    wrapped_message = textwrap.wrap(output_message, width=2000, break_long_words=False,replace_whitespace=False)
     
     # Send each chunk as a separate message, formatted in code blocks
     for chunk in wrapped_message:
@@ -105,5 +121,9 @@ async def whatsnew(ctx):
         
         formatted_chunk = f"```{truncated_chunk}```"
         await ctx.send(formatted_chunk)
-    
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(f'This command is on cooldown, you can use it in {round(error.retry_after, 2)} seconds.')
+
 bot.run(APP_TOKEN)
